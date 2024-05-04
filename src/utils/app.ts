79146b4c3d1b7dd14e712632e25bc25449927b38
it21012488 <it21012488@my.sliit.lk>
2024-05-04 15:05:53 +0530

    add hello world route

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import routes from "../routes";
 
 export const app: Application = express();
@@ -8,3 +8,7 @@ app.get("/health-check", (req, res) => res.sendStatus(200));
 // This route is used to crash the server for demostration purposes 
 app.get("/crash", () => process.exit(1));
 app.use("/api/v1", routes);
+
+app.get("/hello-world", (req: Request, res: Response) => {
+    res.status(200).json({ message: "Hello World!" })
+});
